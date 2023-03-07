5639b7e3db677a5bf1ad4175a2c895ea6b4d3986
hawolt <45050975+hawolt@users.noreply.github.com>
2023-03-07 03:31:52 +0100

    removed obsolete exception

--- src/main/java/com/hawolt/Main.java
+++ src/main/java/com/hawolt/Main.java
@@ -9,7 +9,6 @@ import com.hawolt.util.ReflectHttp;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -31,8 +30,6 @@ public class Main {
             SocketServer.launch();
             Netherblade.create();
             RuleInterpreter.reload(null);
-        } catch (FileNotFoundException e) {
-            System.err.println("instructions.json not present.");
         } catch (NoSuchFieldException | IllegalAccessException e) {
             Logger.error(e);
             System.err.println("Unable to modify permitted HTTP methods, exiting (1).");
