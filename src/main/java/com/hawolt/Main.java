3ec79609d02aa8196b8740852320d464878deea8
hawolt <45050975+hawolt@users.noreply.github.com>
2023-03-08 11:47:23 +0100

    added capability of intercepting xmpp traffic in a very basic way, still requires settings rewrites

--- src/main/java/com/hawolt/Main.java
+++ src/main/java/com/hawolt/Main.java
@@ -2,14 +2,19 @@ package com.hawolt;
 
 import com.hawolt.http.LocalExecutor;
 import com.hawolt.logger.Logger;
+import com.hawolt.mitm.cache.InternalStorage;
 import com.hawolt.mitm.rule.RuleInterpreter;
+import com.hawolt.socket.SocketMessageCallback;
+import com.hawolt.socket.SocketProxy;
 import com.hawolt.socket.SocketServer;
 import com.hawolt.ui.Netherblade;
 import com.hawolt.util.ReflectHttp;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Base64;
 
 /**
  * Created: 30/07/2022 15:37
@@ -30,6 +35,28 @@ public class Main {
             SocketServer.launch();
             Netherblade.create();
             RuleInterpreter.reload(null);
+            //TODO experimental
+            InternalStorage.registerStorageListener(true, "xmpptoken", xmpptoken -> {
+                JSONObject object = new JSONObject(new String(Base64.getDecoder().decode(xmpptoken.split("\\.")[1])));
+                String affinity = object.getString("affinity");
+                InternalStorage.registerStorageListener(false, affinity, host -> {
+                    Logger.info("[XMPP] relaying to {}", host);
+                    SocketProxy proxy = new SocketProxy(new SocketMessageCallback() {
+                        @Override
+                        public byte[] onOutgoing(byte[] b) {
+                            Logger.debug("XMPP-OUT {}", new String(b));
+                            return b;
+                        }
+
+                        @Override
+                        public byte[] onIngoing(byte[] b) {
+                            Logger.error("XMPP-IN {} ", new String(b));
+                            return b;
+                        }
+                    }, host, 5223, 5223);
+                    proxy.start();
+                });
+            });
         } catch (NoSuchFieldException | IllegalAccessException e) {
             Logger.error(e);
             System.err.println("Unable to modify permitted HTTP methods, exiting (1).");
