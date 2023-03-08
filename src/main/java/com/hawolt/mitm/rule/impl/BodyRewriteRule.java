1cefe23f18bed25c58b296ae7fd1a433007e2caf
hawolt <45050975+hawolt@users.noreply.github.com>
2023-03-08 11:45:22 +0100

    add cache support to body-mitm code

--- src/main/java/com/hawolt/mitm/rule/impl/BodyRewriteRule.java
+++ src/main/java/com/hawolt/mitm/rule/impl/BodyRewriteRule.java
@@ -1,6 +1,8 @@
 package com.hawolt.mitm.rule.impl;
 
+import com.hawolt.http.proxy.BasicProxyServer;
 import com.hawolt.logger.Logger;
+import com.hawolt.mitm.cache.InternalStorage;
 import com.hawolt.mitm.rule.AbstractRewriteRule;
 import com.hawolt.mitm.rule.Replacement;
 import com.hawolt.mitm.rule.ReplacementGroup;
@@ -20,25 +22,34 @@ import java.util.stream.Collectors;
  **/
 
 public class BodyRewriteRule extends AbstractRewriteRule<String, String> {
-    private final String plain;
+    private String replacement, plain, name;
     private List<ReplacementGroup> groups;
-    private String replacement;
     private Pattern pattern;
+    private int key, value;
 
     public BodyRewriteRule(JSONObject object) {
         super(object);
-        this.plain = object.getString("find");
-        if (type != RuleType.REGEX) {
-            this.replacement = object.getString("replace");
+        if (type != RuleType.PLAINTEXT_CACHE) {
+            this.plain = object.getString("find");
+            if (type == RuleType.PLAIN) {
+                this.replacement = object.getString("replace");
+            } else {
+                this.pattern = Pattern.compile(plain);
+                if (type == RuleType.REGEX_CACHE) {
+                    this.key = object.getInt("key");
+                    this.value = object.getInt("value");
+                } else if (getType() == RuleType.REGEX) {
+                    this.groups = object.getJSONArray("groups")
+                            .toList()
+                            .stream()
+                            .map(o -> (HashMap<?, ?>) o)
+                            .map(JSONObject::new)
+                            .map(ReplacementGroup::new)
+                            .collect(Collectors.toList());
+                }
+            }
         } else {
-            this.pattern = Pattern.compile(plain);
-            this.groups = object.getJSONArray("groups")
-                    .toList()
-                    .stream()
-                    .map(o -> (HashMap<?, ?>) o)
-                    .map(JSONObject::new)
-                    .map(ReplacementGroup::new)
-                    .collect(Collectors.toList());
+            this.name = object.getString("name");
         }
     }
 
@@ -60,6 +71,9 @@ public class BodyRewriteRule extends AbstractRewriteRule<String, String> {
 
     @Override
     public String rewrite(String in) {
+        Matcher matcher;
+        StringBuilder builder;
+        List<Replacement> list;
         String modified = null;
         switch (type) {
             case PLAIN:
@@ -68,9 +82,9 @@ public class BodyRewriteRule extends AbstractRewriteRule<String, String> {
                 Logger.debug("[{}-RULE] {} -> {}", type.name(), plain, getReplacement());
                 break;
             case REGEX:
-                Matcher matcher = pattern.matcher(in);
-                StringBuilder builder = new StringBuilder(in);
-                List<Replacement> list = new ArrayList<>();
+                matcher = pattern.matcher(in);
+                builder = new StringBuilder(in);
+                list = new ArrayList<>();
                 while (matcher.find()) {
                     for (int i = 1; i <= matcher.groupCount(); i++) {
                         for (ReplacementGroup group : getGroups()) {
@@ -86,10 +100,24 @@ public class BodyRewriteRule extends AbstractRewriteRule<String, String> {
                     Replacement rule = list.get(i);
                     String target = builder.substring(rule.getStart(), rule.getEnd());
                     builder.replace(rule.getStart(), rule.getEnd(), rule.getReplacement());
-                    Logger.error("[REWRITE] {} -> {}", target, rule.getReplacement());
+                    Logger.info("[REWRITE] {} -> {}", target, rule.getReplacement());
                 }
                 in = builder.toString();
                 Logger.debug("[{}-RULE] {}:{} -> {}", type.name(), plain, target, getReplacement());
+                break;
+            case PLAINTEXT_CACHE:
+                InternalStorage.add(name, in);
+                break;
+            case REGEX_CACHE:
+                matcher = pattern.matcher(in);
+                while (matcher.find()) {
+                    int count = matcher.groupCount();
+                    if (count >= Math.max(key, value)) {
+                        String k = matcher.group(key);
+                        String v = matcher.group(value);
+                        InternalStorage.add(k, v);
+                    }
+                }
                 break;
             case UNKNOWN:
                 break;
