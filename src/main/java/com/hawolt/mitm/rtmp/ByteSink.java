62c5c85096e790dc75924a148e67335487600980
hawolt <45050975+hawolt@users.noreply.github.com>
2023-05-01 19:54:05 +0200

    remove unused helper classes

--- src/main/java/com/hawolt/mitm/rtmp/ByteSink.java
+++
@@ -1,8 +0,0 @@
-package com.hawolt.mitm.rtmp;
-
-import java.io.IOException;
-import java.io.OutputStream;
-
-public interface ByteSink {
-    void drain(OutputStream outputStream) throws IOException;
-}
